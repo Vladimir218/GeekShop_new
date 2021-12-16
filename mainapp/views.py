f2d6c1491ee1e4d1233fa5600de0eabec1d07334
Vladimir <kvv23@tut.by>
2021-12-17 00:10:11 +0300

    lesson_14. Установлен debug-tool-bar и выполнена оптимизация запросов в контроллере с использованием select_related

--- mainapp/views.py
+++ mainapp/views.py
@@ -24,14 +24,21 @@ def get_basket(user):
         return []
 
 
-def get_hot_product():
-    products = Product.objects.filter(is_active=True, category__is_active=True)
-    return random.sample(list(products), 1)[0]
+# def get_hot_product():
+    # products = Product.objects.filter(is_active=True, category__is_active=True)
+    # return random.sample(list(products), 1)[0]
+    
+def get_hot_product_list():    
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+    hot_product = random.sample(list(products), 1)[0]
+    hot_list = products.exclude(pk=hot_product.pk)[:3]
+    return (hot_product, hot_list)
 
 
-def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
-    return same_products
+# def get_same_products(hot_product):
+#     same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
+#     return same_products
+    
 
 
 def products(request, pk=None, page=1):
@@ -66,8 +73,11 @@ def products(request, pk=None, page=1):
             # "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
+    # hot_product = get_hot_product()
+    # same_products = get_same_products(hot_product)
+   
+    hot_product, same_products = get_hot_product_list()
+    
     content = {
         "title": title,
         "links_menu": links_menu,
